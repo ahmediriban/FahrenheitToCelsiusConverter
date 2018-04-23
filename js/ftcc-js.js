04d292c595b47691225c85d43e080200e08c9de2
ahmediriban <ahmedireban@gmail.com>
2018-04-23 17:33:17 +0300

    Add files via upload

---
+++ js/ftcc-js.js
@@ -0,0 +1,19 @@
+/*
+Script here
+Ahmed AbuIriban
+ */
+var value;
+var option;
+if(document.getElementById('option-').getElementsByTagName('option')[0].selected=true){
+    document.getElementById('s1').innerHTML="C";
+}
+else if(document.getElementById('option-').getElementsByTagName('option')[1].selected=true){
+    document.getElementById('s1').innerHTML="F";
+}
+if(document.getElementById('option-').getElementsByTagName('option')[0].selected=true){
+    document.getElementById('s2').innerHTML="F";
+}
+else if(document.getElementById('option-').getElementsByTagName('option')[1].selected=true){
+    document.getElementById('s2').innerHTML="C";
+}
+document.getElementById('s1').innerHTML="F";
